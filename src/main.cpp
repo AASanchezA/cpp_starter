bbc4ff3365b0041e9e8c810eb3f7fc0d37b9eabc
AASanchezA <sanchezandres@siemens.com>
2022-10-17 17:28:39 +0200

    chore: add utils

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,15 @@
 #include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
 
+#include <Poco/NotificationCenter.h>
+#include <Poco/Notification.h>
+#include <Poco/Observer.h>
+#include <Poco/NObserver.h>
+#include <Poco/AutoPtr.h>
+#include <Poco/UUID.h>
+#include <Poco/UUIDGenerator.h>
+#include <Poco/Base64Encoder.h>
+
 // This file will be generated automatically when you run the CMake configuration step.
 // It creates a namespace called `cpp_starter`.
 // You can modify the source template at `configured_files/config.hpp.in`.
@@ -55,6 +64,27 @@ void quicksort(std::vector<int> &arr, Limit limit)// NOLINT(misc-no-recursion,-w
   quicksort(arr, Limit {pivotIndex + 1, limit.high});
 }
 
+
+class BaseNotification: public Poco::Notification {};
+class SubNotification : public BaseNotification {};
+
+namespace n {
+class Target {
+  public:
+  // NOLINTNEXTLINE
+  void handleBase(BaseNotification* pNf) { // cppcheck-suppress functionStatic
+
+    fmt::print(" handleBase: {}\n", pNf->name());
+    pNf->release();
+  }
+
+  // NOLINTNEXTLINE
+  void handleSub(const Poco::AutoPtr<SubNotification> &pNf) // cppcheck-suppress functionStatic
+  {
+    fmt::print(" handelSub: {}\n", pNf->name());
+  }
+};
+}
 
 // NOLINTNEXTLINE(bugprone-exception-escape)
 int main(int argc, const char **argv)
@@ -125,6 +155,32 @@ int main(int argc, const char **argv)
     json list = json::array();
 
     // for (const auto &num : the_queue) { fmt::print("number: '{}'\n", num); }
+
+
+    Poco::UUIDGenerator& generator = Poco::UUIDGenerator::defaultGenerator();
+
+    Poco::UUID uuid1(generator.create());
+    Poco::UUID uuid2(generator.createRandom());
+    Poco::UUID uuid3(generator.createFromName(Poco::UUID::uri(), "http://appinf.com"));
+
+    fmt::print("uuid1:  {}\n", uuid1.toString());
+    fmt::print("uuid2:  {}\n", uuid2.toString());
+    fmt::print("uuid3:  {}\n", uuid3.toString());
+
+    Poco::NotificationCenter notCenter;
+    n::Target target;
+
+    notCenter.addObserver(Poco::Observer<n::Target, BaseNotification>(target, &n::Target::handleBase));
+    notCenter.addObserver(Poco::NObserver<n::Target, SubNotification>(target, &n::Target::handleSub));
+
+    notCenter.postNotification(new BaseNotification);
+    notCenter.postNotification(new SubNotification);
+
+    notCenter.removeObserver(Poco::Observer<n::Target, BaseNotification>(target, &n::Target::handleBase));
+    notCenter.removeObserver(Poco::NObserver<n::Target, SubNotification>(target, &n::Target::handleSub));
+
+    Poco::Base64Encoder encoder(std::cout);
+    encoder << "Hello Andres";
 
   } catch (const std::exception &e) {
     spdlog::error("Unhandled exception in main: {}", e.what());
